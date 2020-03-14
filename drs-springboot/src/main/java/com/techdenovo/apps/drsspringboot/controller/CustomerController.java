d6775b0f5dbf3846b2aebe4d4e6d9e1cb07309e5
Kalyan Mazumder <kalyaninform@gmail.com>
2020-03-14 15:10:16 +0530

    Added Form and controller updated

--- drs-springboot/src/main/java/com/techdenovo/apps/drsspringboot/controller/CustomerController.java
+++ drs-springboot/src/main/java/com/techdenovo/apps/drsspringboot/controller/CustomerController.java
@@ -1,7 +1,10 @@
 package com.techdenovo.apps.drsspringboot.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,10 +18,26 @@ public class CustomerController {
 	@Autowired
 	CustomerService customerService;
 	
+	@RequestMapping("/")
+	public String viewHomePage(Model model) {
+	    List<Customer> listCustomers = customerService.listAll();
+	    model.addAttribute("listCustomers", listCustomers);
+	     
+	    return "index";
+	}
+	
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public String saveCustomer(@ModelAttribute("customer") Customer customer) {
 		customerService.saveCustomer(customer);
 	     
 	    return "redirect:/";
+	}
+	
+	@RequestMapping("/new")
+	public String showNewProductPage(Model model) {
+	    Customer customer = new Customer();
+	    model.addAttribute("customer", customer);
+	     
+	    return "add-customer";
 	}
 }
--- drs-springboot/src/main/java/com/techdenovo/apps/drsspringboot/service/CustomerService.java
+++ drs-springboot/src/main/java/com/techdenovo/apps/drsspringboot/service/CustomerService.java
@@ -1,5 +1,7 @@
 package com.techdenovo.apps.drsspringboot.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,6 +15,10 @@ public class CustomerService {
 	
 	@Autowired
 	CustomerRepository customerRepo;
+	
+	public List<Customer> listAll() {
+        return customerRepo.findAll();
+    }
 
 	public void saveCustomer(Customer customer) {
 		customerRepo.save(customer);
